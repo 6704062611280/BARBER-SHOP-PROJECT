473cccdc044484d2b22f5bca80f4f7aaaf039c03
Akkadecht <jayjamesnew4@gmail.com>
2026-03-13 19:36:49 +0700

    add css in jsx file

--- frontend/src/component/OTPPage.jsx
+++ frontend/src/component/OTPPage.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom"
 import { useState, useRef,useContext  } from "react"
 import { DataContext } from "../DataContext"
-
+import "./style/OTPPage.css"
 
 export default function OTPPage(){
 
